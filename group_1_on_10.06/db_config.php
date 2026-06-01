4ddaae7ce173694c828348178007f1db03bd22a1
Arvesso <arvesso@icloud.com>
2026-06-01 16:26:20 +0500

    restruct

--- group_1_on_10.06/db_config.php
+++
@@ -1,6 +0,0 @@
-<?php
-return [
-    'dsn' => 'mysql:host=MySQL-8.0;dbname=artem;charset=utf8mb4',
-    'user' => 'root',
-    'password' => ''
-];
--- group_2_on_11.06/db_config.php
+++
@@ -1,6 +0,0 @@
-<?php
-return [
-    'dsn' => 'mysql:host=MySQL-8.0;dbname=DemEx;charset=utf8mb4',
-    'user' => 'root',
-    'password' => ''
-];
